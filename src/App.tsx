e7237a8939e8493507185a176a9c2053cfc1d14b
Woll2 <152017772+Woll2@users.noreply.github.com>
2024-11-30 13:28:21 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -141,16 +141,7 @@ function App() {
           <div className="ButtonsSection">
             {!connected ? (
               <div className="ConnectButtonWrapper">
-                <TonConnectButton style={{
-                  backgroundColor: '#000000',
-                  color: '#000000',
-                  boxShadow: '0px 0px 1px 1px #00ff66',
-                  padding: '1px 100px',
-                  display: 'flex',
-                  alignItems: 'center',
-                  gap: '4px',
-                  height: '40px',
-                }} />
+                <TonConnectButton >
               </div>
             ) : (
               <button 
